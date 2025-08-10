4ee56768ba4654a7c70679cdf76933feeb55246a
pkamau36 <128474810+pkamau36@users.noreply.github.com>
2025-08-10 19:06:53 +0300

    week two assignment

---
+++ my_list.py
@@ -0,0 +1,31 @@
+# empty list
+my_list = []
+
+# append elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# insert 15 at the 2nd position
+my_list.insert(1, 15)
+
+# extend the list with another list
+my_list.extend([50, 60, 70])
+
+# remove the last element from the list
+my_list.pop()
+
+# sort the list in ascending order
+my_list.sort()
+
+# find and print the index and value of 30 
+index_of_30 = my_list.index(30)
+print("my list:", my_list)
+print("Index of 30:", index_of_30)
+
+
+
+
+
+
