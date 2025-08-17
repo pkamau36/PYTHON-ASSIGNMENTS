772c1f2baf1bf71cc04ad386cdb005dd52a02a0f
pkamau36 <128474810+pkamau36@users.noreply.github.com>
2025-08-17 19:11:31 +0300

    Week three assignment

---
+++ calculate_discount.py
@@ -0,0 +1,30 @@
+# function named calculate_discount that uses original price(price) and percentage discount(discount_percent) as parameters.
+
+def calculate_discount(price, discount_percent):
+    # check if the percentage discount is 20 or more:
+    if discount_percent>= 20:
+        # calculate the discount amount
+        discount_amount = price * (discount_percent / 100)
+        final_price = price - discount_amount
+        # return the final price after discount
+        return final_price
+    #if percentage is less than 20, return the original price
+    else:
+        return price
+    
+# prompt the user for the original price and discount percentage
+print("welcome to the Discount Calculator!")
+price = float(input("Enter the original price: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+
+# call the function and print the final price
+final_price = calculate_discount(price, discount_percent)
+ 
+if discount_percent >= 20:
+    print(f"The final price is: {final_price:.2f}")
+else:
+    print(f"No discount applied. The final price is: {price:.2f}")
+
+    print("Thank you for using the Discount Calculator!")
+
